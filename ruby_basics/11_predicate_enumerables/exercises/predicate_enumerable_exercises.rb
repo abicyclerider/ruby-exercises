d4d05300ffa1cb395f017beaf10bd3810f3e808d
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-05-08 06:16:00 -0600

    Finish up to 21st century

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,13 +1,16 @@
 def coffee_drink?(drink_list)
   # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  drink_list.include?("coffee") || drink_list.include?("espresso")
 end
 
 def correct_guess?(guess_list, answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+  guess_list.any? {|guest| guest == answer}
 end
 
 def twenty_first_century_years?(year_list)
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
+  year_list.all? { |year| year > 2000 && year < 2101 }
 end
 
 def correct_format?(word_list)
